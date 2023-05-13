ba99595f02acb725b7778cfd9750e7aad2013e17
Utilizator <Stanislavbox24@gmail.com>
2023-05-13 12:03:27 +0300

    spinner adjustment

--- src/js/spinner.js
+++ src/js/spinner.js
@@ -1,4 +1,4 @@
 export function spinnerFoo(){
-  const spinner = document.querySelector('.book');
+  const spinner = document.querySelector('.js-spinner-wrap');
   spinner.classList.toggle('visual-hidden')
 }
