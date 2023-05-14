8c6b484e3ce2c52adb6c0e67d65dfb5ceed98bb7
Dmytro Berezhnoi <berezhnoi.dmytro@gmail.com>
2023-05-14 16:08:58 +0200

    add scroll-to-top button + fix scroll bar

--- src/js/categories-book/renderCategoriesListMarkUp.js
+++ src/js/categories-book/renderCategoriesListMarkUp.js
@@ -9,7 +9,7 @@ export async function renderCategoriesListMarkUp() {
     arrayOfCategories.forEach(dataItem => {
       const { list_name } = dataItem;
 
-      const htmlCategoriesList = `<li>
+      const htmlCategoriesList = `<li class="categories-list-item">
   <button type="button" class="category-btn" data-id="${dataItem.list_name}">${dataItem.list_name}</button>
 </li>`;
 
