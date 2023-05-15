81f783140321067db0390d449d606c5ab0a1d67b
Dmytro Berezhnoi <berezhnoi.dmytro@gmail.com>
2023-05-15 20:25:05 +0200

    add css rule pointer-events: none + add class js-book-list

--- src/js/categories-book/allBooksCategoryMarkup.js
+++ src/js/categories-book/allBooksCategoryMarkup.js
@@ -2,7 +2,7 @@ import { createBookMarkup } from './oneBookMarkup';
 
 export function createCategoryBooksMarkup(data) {
   return `
-      <ul class="category-books-list">
+      <ul class="category-books-list js-book-list">
         ${data
           .map(element => {
             return `
