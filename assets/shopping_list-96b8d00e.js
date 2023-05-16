1589dd85e9276839b95e9bbbb0415c05c5bde570
Stanislavbox <Stanislavbox24@gmail.com>
2023-05-16 12:27:09 +0000

    Deploying to gh-pages from @ Stanislavbox/project-REACTIVE_DEVS@056a46e0528bbea9b0509040bf3947879a31369c 🚀

--- assets/shopping_list-96b8d00e.js
+++ assets/shopping_list-96b8d00e.js
@@ -1,4 +1,4 @@
-import{h as l,L as u,s as n,i as d,o as g,d as m,l as h,e as k,f as b,g as y,c as L}from"./footer-477c0461.js";const p={shopListEmpty:document.querySelector(".shopping-list-empty"),shopListWrapper:document.querySelector(".shopping-list-card"),shopList:document.querySelector("#booksShopingList"),startMarkup:`<p class="shopping-list-empty-message">
+import{h as l,L as u,s as n,i as d,o as m,d as g,l as h,e as k,f as b,g as y,c as L}from"./footer-477c0461.js";const p={shopListEmpty:document.querySelector(".shopping-list-empty"),shopListWrapper:document.querySelector(".shopping-list-card"),shopList:document.querySelector("#booksShopingList"),startMarkup:`<p class="shopping-list-empty-message">
     This page is empty, add some books and proceed to order.
   </p>
   <picture class="shopping-list-empty-img">
@@ -15,7 +15,7 @@ import{h as l,L as u,s as n,i as d,o as g,d as m,l as h,e as k,f as b,g as y,c a
       media="(max-width:767px)"
     />
     <img src="./images/books-mob-min.png" alt="books" />
-  </picture>`};async function S(){let o=l.load(u);if(console.log(JSON.parse(o).length>0),!JSON.parse(o).length>0||!o)return n(),p.shopListEmpty.insertAdjacentHTML("beforeend",p.startMarkup);p.shopListEmpty.style.display="none",o=JSON.parse(o);let t=[];for(let s=0;s<o.length;s+=1){const e=await d(o[s]);t.push(e)}const r=t.map(({_id:s,author:e,book_image:a,description:c,title:i})=>`<button class="popup-book-close-btn js-popup-close" data_id=${s} type="button">
+  </picture>`};async function S(){let o=l.load(u);if(!JSON.parse(o).length>0||!o)return n(),p.shopListEmpty.insertAdjacentHTML("beforeend",p.startMarkup);p.shopListEmpty.style.display="none",o=JSON.parse(o);let t=[];for(let s=0;s<o.length;s+=1){const e=await d(o[s]);t.push(e)}const r=t.map(({_id:s,author:e,book_image:a,description:c,title:i})=>`<button class="popup-book-close-btn js-popup-close" data_id=${s} type="button">
       <svg class="popup-book-close-icon js-popup-close" width="24" height="24">
         <use class="js-popup-close" href="./img/sprite.svg#icon-shopping"></use>
       </svg>
@@ -23,9 +23,9 @@ import{h as l,L as u,s as n,i as d,o as g,d as m,l as h,e as k,f as b,g as y,c a
     <div class="popup-book-wrapper">
       <img class="popup-book-cover" src="${a}"" alt="Book's cover. ${i}" />
       <div class="popup-book-inner">
-        <h2 class="popup-book-title">${i}"</h2>
-        <h3 class="popup-book-author">${e}"</h3>
-        <p class="popup-book-description">${c}"</p>
+        <h2 class="popup-book-title">${i}</h2>
+        <h3 class="popup-book-author">${e}</h3>
+        <p class="popup-book-description">${c}</p>
         <ul class="list popup-book-shops-list">
           <li class="popup-book-shops-item">
             <a
@@ -47,4 +47,4 @@ import{h as l,L as u,s as n,i as d,o as g,d as m,l as h,e as k,f as b,g as y,c a
           </li>
         </ul>
       </div>
-    </div>`).join("");n(),p.shopList.insertAdjacentHTML("beforeend",r)}g();const f=document.querySelector(".button-registraition"),v=document.querySelector(".button-login");f.addEventListener("click",m);v.addEventListener("click",h);const x=document.getElementById("logOutButton");x.addEventListener("click",k);const E=document.querySelector(".user_board_signup");E.addEventListener("click",b);const B=document.querySelector(".close-modal");B.addEventListener("click",y);L();S();
+    </div>`).join("");n(),p.shopList.insertAdjacentHTML("beforeend",r)}m();const f=document.querySelector(".button-registraition"),v=document.querySelector(".button-login");f.addEventListener("click",g);v.addEventListener("click",h);const x=document.getElementById("logOutButton");x.addEventListener("click",k);const E=document.querySelector(".user_board_signup");E.addEventListener("click",b);const B=document.querySelector(".close-modal");B.addEventListener("click",y);L();S();
