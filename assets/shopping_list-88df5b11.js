c1acc2a0de03637c0672e38e126af734296e816e
Stanislavbox <Stanislavbox24@gmail.com>
2023-05-17 11:45:19 +0000

    Deploying to gh-pages from @ Stanislavbox/project-REACTIVE_DEVS@da00afe79225d6ed88463ca85757c25e4279a09f 🚀

--- assets/shopping_list-88df5b11.js
+++ assets/shopping_list-88df5b11.js
@@ -14,7 +14,7 @@ import{h as u,L as g,s as r,i as k,n as b,j as y,o as L,p as v,d as f,l as B,e a
       srcset="src/img/books.png 1x, src/img/books.png 2x"
       media="(max-width:767px)"
     />
-    <img src="src/img//books.png" alt="books" />
+    <img src="src/img/books.png" alt="books" />
   </picture>`,arrBtnsID:[]};async function l(){let e=u.load(g);if(e){if(!JSON.parse(e).length)return r(),s.shopListEmpty.insertAdjacentHTML("beforeend",s.startMarkup)}else return r(),s.shopListEmpty.insertAdjacentHTML("beforeend",s.startMarkup);s.shopListEmpty.style.display="none",e=JSON.parse(e);try{let o=[];for(let t=0;t<e.length;t+=1){const i=await k(e[t]);o.push(i)}const p=o.map(({_id:t,author:i,book_image:h,description:d,title:a,list_name:m})=>`<li class="sh-list-item">               
                     <img class="sh-list-book-img" src="${h}"" alt="Book's cover. ${a}" />
                     <div class="sh-list-item-main-wrapper">
